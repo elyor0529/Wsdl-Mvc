128d21b92307926e608c677efae2724a2731004a
Elyor <elyor@outlook.com>
2018-11-20 13:09:33 +0500

    Azure Deploy

---
+++ DemoMvc/App_Start/Startup.Auth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin; 
+
+namespace DemoMvc
+{
+    public partial class Startup
+    {
+        /// <summary>
+        /// For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        /// </summary>
+        /// <param name="app"></param>
+        public void ConfigureAuth(IAppBuilder app)
+        { 
+        }
+    }
+}
---
+++ DemoMvc/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMvc.Filters;
+using DemoMvc.Security;
+using DemoMvc.Services;
+
+namespace DemoMvc.Controllers
+{
+    [SessionAuthorization]
+    public class UserController : Controller
+    {
+        private static readonly UserService _service = new UserService();
+
+        public ActionResult Index()
+        {
+            var error = "";
+            var model = _service.GetByEntity(MembershipHelper.Current.UserName, MembershipHelper.Current.Password, MembershipHelper.Current.Id, ref error);
+
+            if (!string.IsNullOrWhiteSpace(error))
+            {
+                return View("Error", error);
+            }
+
+            return View(model);
+        }
+    }
+}
